d8ad84d8b901472f9b94b1e85c404fdb4db438fd
LabibUW <labib.joarder@gmail.com>
2023-01-12 19:25:20 -0800

    hello

--- shell.c
+++ shell.c
@@ -9,12 +9,15 @@
 
 #define MAX_LINE 1024
 
+int done = 0;
+void ParseInput (char* input, char** args);
+
 int main(void)
 {
     char *args[MAX_LINE/2 + 1]; /* command line arguments */
     int should_run = 1; /* flag to determine when to exit program */
     while (should_run) {
-        printf("osh> ");
+        printf("osh>");
         fflush(stdout);
         /**
         * After reading user input, the steps are:
@@ -22,8 +25,6 @@ int main(void)
         * (2) the child process will invoke execvp()
         * (3) parent will invoke wait() unless command included &
         */
-        
-        /* Start of Copy GETTING ARGUMENTS FROM CMD*/
 
         char *temp;
         char *input;
@@ -31,43 +32,71 @@ int main(void)
         char *buffer = NULL;
         ssize_t read; // long
         read = getline(&buffer, &buffer_size, stdin);
-        printf("line: %s", buffer);
+        // printf("line: %s", buffer);
         
         // If input is length 0 or less.
         if(strlen(buffer) <= 0) 
         {
-            return 0;
+            return 0; // fix
         } 
         // Input has length 1 or greater
         else 
         {
-            // Duplicate command
+            // Duplicate input
             input = strdup(buffer);
 
-            // Parse command!
+            // Parse input!
             ParseInput(input, args); //ParseInput (input string, args[] an array)
 
             // Check if History as been called '!!'
+            if(strcmp(args[0], "!!")) {
+                // RunHistoryCommand(); // TODO: 
+            }
+            
 
             // Do we need to redirect? parameters (char ** args)
             // Do we need to pipe? (char ** args)
-            // TODO: For loop through all of the args[] and strcmp(args[i], "|")
-            // TODO: For loop through all of the args[] and strcmp(args[i], "<")
-            // TODO: For loop through all of the args[] and strcmp(args[i], ">")
+            // For loop through all of the args[] and strcmp(args[i], "|")
+            for(int i = 0; args[i] != NULL; i++) {
+                if(strcmp(args[i], "|")) {
+                    // RunPipeCommand(); // TODO:
+                }
+            }
 
+            // For loop through all of the args[] and strcmp(args[i], "<")
+            for(int i = 0; args[i] != NULL; i++) {
+                if(strcmp(args[i], "<")) {
+                    // RunInputCommand(); // TODO:
+                }
+            }
+            // For loop through all of the args[] and strcmp(args[i], ">")
+            for(int i = 0; args[i] != NULL; i++) {
+                if(strcmp(args[i], ">")) {
+                    // RunOutputCommand(); // TODO:
+                }
+            }
+
+            // TODO: Execute command
+            if(done == 0) {
+                int rc = execvp(args[0], args);
+            }
+
+
+            // Check for 'exit'
+            for(int i = 0; args[i] != NULL; i++) {
+                if(strcmp(args[i], "exit")) {
+                    // RunExitCommand(); // TODO:
+                }
+            }
+            
 
         }
 
-        
-
-        
         // duplicated from strdup
         free(input);
         
         // resized by getline
         free(buffer);
-
-        /* End of Copy GETTING ARGUMENTS FROM CMD*/
 
         should_run = 0;
 
@@ -77,7 +106,26 @@ int main(void)
 
 
 // TODO: Parse command!
-void ParseCommand (char* input, char** args) {
-    
-    // Use tokenizeEx to help!!
+void ParseInput (char* input, char** args) {
+    int count = 0;
+    char *pch;
+
+    pch = strtok(input, " ");
+    while(pch != NULL) {
+        // printf("Token %d: %s\n", count, pch);
+        args[count] = pch;
+        ++count;
+
+        pch = strtok(NULL, " ");
+    }
+
+    for(int i = 0; args[i] != NULL; i++) {
+        printf("%s ", args[i]);
+    }
+    args[count] = NULL;
+
+    int len = strlen(args[count-1]);
+    args[count-1][len-1] = '\0';
+
+    // Use tokenizeEx to help!! 
 }
